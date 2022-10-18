f4f8e9de2ea009bb7fd7efd20cfa611d2b8f65c2
Francois18 <79960710+Francois18@users.noreply.github.com>
2022-10-18 23:38:19 +0300

    Printf function

---
+++ _printf.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
+/**
+ * _printf -  the printf function
+ * @format: string with format specifier
+ * Return: haracters printed
+ */
+int _printf(const char * format, ...)
+{
+	int i, p = 0;
+	int (*m)(va_list);
+	va_list args;
+
+	va_start(args, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			m = get_func(format[i + 1]);
+			p = m(args);
+			i += 2;
+		}
+		_putchar(format[i]);
+		i++;
+	}
+	_putchar('\n');
+	va_end(args);
+	return(p);
+}
